f0d120b8d801f0c5b21ae6ad04f12f3592471d47
Ashish Bhungani <bhunganiashish@gmail.com>
2024-03-17 13:33:41 +0530

    A

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 const RPC =  "https://testnet-rpc.genesys.network";
 const NFTABI = require("./src/Nft.json");
 const NFTSTAKEABI = require("./src/Stake.json");
-const NftContractAddress = "0x36F2D2787A5cCF389087d9b03698be16244E6403";
+const NftContractAddress = "0x6527b899F9DafC699DB349BFEC0499fEb0E72FdB";
 const NftStakeContract = "0x2c523E009ef564f5e75cb8477F01A623e6faBF39"
 
 module.exports  =  { 
